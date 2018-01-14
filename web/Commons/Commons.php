3e9822162864b552f1924ed5889c793b99dbebf2
Julia Sobrado Jiménez <julia90sj@gmail.com>
2018-01-14 22:08:40 +0100

    Añadido HTML del footer común.

--- web/Commons/Commons.php
+++ web/Commons/Commons.php
@@ -2,7 +2,19 @@
 class  Commons{
 
 	public static function footer(){
-		return 'h';
+		$footerHTML = "
+		<footer class= 'l col12'>
+			<div class='content'>
+				<div>
+					<h4>Contacto</h4>
+					<a href='mailto:contacto@learn.com'>contacto@learn.com</a>
+				</div>
+				<div id='copy'>
+					<span>&copy; L-earn " . date("Y") . "</span>
+				</div>
+			</div>
+		</footer>"
+		return $footerHTML;
 	}
 }
 
