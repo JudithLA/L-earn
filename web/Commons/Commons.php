e47fb9bfe3e72b1849f8062d081a2bc6d449cd01
Julia Sobrado Jiménez <julia90sj@gmail.com>
2018-01-24 23:10:55 +0100

    Añadido el headerStudent a Commons.php y modificada la fecha del footer.

--- web/Commons/Commons.php
+++ web/Commons/Commons.php
@@ -10,7 +10,7 @@ class  Commons{
 					<a href='mailto:contacto@learn.com'>contacto@learn.com</a>
 				</div>
 				<div id='copy'>
-					<span>&copy; L-earn " . date("Y") . "</span>
+					<span>&copy; L-earn 2017-" . date("Y") . "</span>
 				</div>
 			</div>
 		</footer>";
@@ -33,6 +33,29 @@ class  Commons{
 		";
 	return $teacherHeaderHTML;
 	}
+
+	public static function headerStudent(){
+		$headerHTML = "
+		<header>
+			<div class='content'>
+				<a href='http://localhost:8888/L-earn/web/homeStudent/homeStudent.php'>
+					<h1>L-EARN</h1>
+				</a>
+				<nav>
+					<ul>
+						<li><a href='#'>Tests</a></li>
+						<li><a href='http://localhost:8888/L-earn/web/rewardsStudent/rewardsStudent.php'>Recompensas</a></li>
+					</ul>
+				</nav>
+				<span id='headPoints'>" . $_SESSION['finPoints'] . "</span>
+				<span id='headPercent'>" . $_SESSION['percentage'] . "</span>
+				<span>" . $_SESSION['nameStu'] . $_SESSION['surnameStu'] . "</span>
+				<img src='" . $_SESSION['imgStu'] . "' alt='Imagen del usuario'>
+			</div>
+		</header>";
+		return $headerHTML;
+	}
+
 }
 
 ?>
