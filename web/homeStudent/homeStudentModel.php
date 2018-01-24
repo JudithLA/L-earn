73ace117cadb259bb781d06b3af4d77751f3bde0
Julia Sobrado Jiménez <julia90sj@gmail.com>
2018-01-24 13:13:10 +0100

    Actualizada la homeStudent.

--- web/homeStudent/homeStudentModel.php
+++ web/homeStudent/homeStudentModel.php
@@ -16,13 +16,25 @@
 
 			$this->mysqli = new MysqlDBImplementation("127.0.0.1", "8889", "DBLEARN", "root", "learn");
 
-
-
-
 		}
 
 		//Destructor (destruye la clase)
 		public function __destruct(){}
+
+		public function infoStudent(){
+
+			$_SESSION['id'] = 1;
+
+			$consult = "SELECT NOMBRE_ALUMN, CONCAT(SUBSTRING(APELLIDO_ALUMN, 1, 1), '.') AS FirstSurname, IMG_ALUMN FROM ALUMN WHERE ID_ALUMN = '{$_SESSION["id"]}'";
+
+			$result = $this->mysqli -> executeQuery($consult);
+
+			$_SESSION['nameStu'] = $result[0]['NOMBRE_ALUMN'];
+			$_SESSION['surnameStu'] = $result[0]['FirstSurname'];
+			$_SESSION['imgStu'] = $result[0]['IMG_ALUMN'];
+
+
+		}
 
 		// Método consultar la BBDD y obtener el último test final realizado por el alumno
 		// Habrá que pasar el ID_ALUMN por parámetro: public function nextTest($idAlumno){WHERE ALUMN.ID_ALUMN = '{$idAlumno}'}
@@ -53,17 +65,17 @@
 			$_SESSION['id'] = 1;
 			// Definimos la query
 			// Habrá que modificar el ID_ALUMN por '{$_SESSION ["id"]}'
-			$consult = "SELECT TRUNCATE((ALUMN.EXPERIENCIA_ALUMN*100)/(SELECT COUNT(ENTRE.ID_ENTRE)*4 AS TotalExperiencia FROM ENTRE
+			$consult = "SELECT CONCAT(TRUNCATE((ALUMN.EXPERIENCIA_ALUMN*100)/(SELECT COUNT(ENTRE.ID_ENTRE)*4 AS TotalExperience FROM ENTRE
         				INNER JOIN TEMAS ON ENTRE.ID_TEMAS = TEMAS.ID_TEMAS
-        				INNER JOIN ASIGN ON TEMAS.ID_ASIGN = ASIGN.ID_ASIGN), 2) AS Porcentaje
+        				INNER JOIN ASIGN ON TEMAS.ID_ASIGN = ASIGN.ID_ASIGN), 2), ' % EXP.') AS Percentage
 						FROM ALUMN WHERE ALUMN.ID_ALUMN = '{$_SESSION["id"]}'";
 
 			// Llamada al método executeQuery() mediante el acceso al atributo $mysqli para ejecutar la query en la BBDD
 			// Almacenamos en $result los resultados de la query "$consult"
 			$result = $this->mysqli -> executeQuery($consult);
 
-			// Almacenamos el resultado de la query (valor del resultado de la operación Porcentaje)
-			$percentage = $result[0]['Porcentaje'];
+			// Almacenamos el resultado de la query (valor del resultado de la operación Percentage)
+			$percentage = $result[0]['Percentage'];
 
 			return $percentage;
 		}
@@ -74,14 +86,14 @@
 			$_SESSION['id'] = 1;
 			// Definimos la query
 			// Habrá que modificar el ID_ALUMN por '{$_SESSION ["id"]}'
-			$consult = "SELECT PUNTOS_ALUMN FROM ALUMN WHERE ID_ALUMN = '{$_SESSION["id"]}'";
+			$consult = "SELECT CONCAT(PUNTOS_ALUMN, ' PTOS.') AS Points FROM ALUMN WHERE ID_ALUMN = '{$_SESSION["id"]}'";
 
 			// Llamada al método executeQuery() mediante el acceso al atributo $mysqli para ejecutar la query en la BBDD
 			// Almacenamos en $result los resultados de la query "$consult"
 			$result = $this->mysqli -> executeQuery($consult);
 
 			// Almacenamos el resultado de la query (únicamente el valor del campo PUNTOS_ALUMN)
-			$finalPoints = $result[0]['PUNTOS_ALUMN'];
+			$finalPoints = $result[0]['Points'];
 
 			return $finalPoints;
 		}
