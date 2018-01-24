31ccc9d9c804a8b04a791c9c34d2b9f93d18bf80
Julia Sobrado Jiménez <julia90sj@gmail.com>
2018-01-24 13:12:24 +0100

    Actualizada la homeStudent.

--- web/homeStudent/homeStudentController.php
+++ web/homeStudent/homeStudentController.php
@@ -1,11 +1,11 @@
-<?php  
+<?php
 	// Incluimos los archivos del Modelo y la Vista
 	require_once __DIR__ . "/homeStudentModel.php";
 	require_once __DIR__ . "/homeStudentView.php";
 
 	// Clase del Controlador
 	class HomeStudentController{
-		
+
 		// Creamos el atributo que almacenará el id del alumno que ha iniciado sesión
 		// private $idAlumno;
 
@@ -13,11 +13,11 @@
 		public function __construct(){
 
 			// Asignamos la variable de sesión con el id del alumno al atributo
-			// $this->idAlumno = $_SESSION['idAlumno'];  
+			// $this->idAlumno = $_SESSION['idAlumno'];
 		}
 
 		//Destructor (destruye la clase)
-		public function __destruct(){}		
+		public function __destruct(){}
 
 		// Método para mostrar el HTML
 		public function viewInfo(){
@@ -31,43 +31,58 @@
 			$datosDeModel = $model->getDatos($this->idAlumno);
 			*/
 
+			$this->getPercentageStudentExpPoints();
+			$this->getStudentFinPoints();
+			$this->getInfoStudent();
+
 			// Instanciamos un objeto de la clase HomeStudentView
 			$view = new HomeStudentView();
 			// Devolvemos el HTML generado en la vista
 			return $view->genHomeStudent();
 		}
 
-		// Método para obtener el último test final realizado por el alumno 
+		public function getInfoStudent(){
+
+			$model = new HomeStudentModel();
+
+			$infoStu = $model->infoStudent();
+
+
+		}
+
+		// Método para obtener el último test final realizado por el alumno
 		public function getNextTest(){
 
 			// Instanciamos un objeto de la clase HomeStudentModel
 			$model = new HomeStudentModel();
 
 			// Devolvemos la info extraída de la BBDD
-			
+
 			return $model->nextTest();
 
 		}
-		
-		// Método para obtener el porcentaje de los puntos de experiencia total del alumno entre todas sus asignaturas 
+
+		// Método para obtener el porcentaje de los puntos de experiencia total del alumno entre todas sus asignaturas
 		public function getPercentageStudentExpPoints(){
 
 			// Instanciamos un objeto de la clase HomeStudentModel
 			$model = new HomeStudentModel();
 
 			// Devolvemos la info extraída de la BBDD
-			return $model->percentageStudentExpPoints();
+			$percentage = $model->percentageStudentExpPoints();
+
+			return $_SESSION['percentage'] = $percentage;
 
 		}
 
-		// Método para obtener los puntos finales totales del alumno 
+		// Método para obtener los puntos finales totales del alumno
 		public function getStudentFinPoints(){
 
-			// Instanciamos un objeto de la clase HomeStudentModel
 			$model = new HomeStudentModel();
 
-			// Devolvemos la info extraída de la BBDD
-			return $model->studentFinPoints();
+			$finPoints = $model->studentFinPoints();
+
+			return $_SESSION['finPoints'] = $finPoints;
 
 		}
 
